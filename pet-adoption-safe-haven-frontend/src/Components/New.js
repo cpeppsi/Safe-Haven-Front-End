09bebb2d386daf3c98ca1dc928cc3d6a175129e7
Mayna-B <me2be2014@gmail.com>
2023-07-13 23:55:12 -0400

    adding updated form data

--- pet-adoption-safe-haven-frontend/src/Components/New.js
+++ pet-adoption-safe-haven-frontend/src/Components/New.js
@@ -1,51 +1,170 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
 
-function New() {
-const navigate = useNavigate()
 
-const [petInput, setPetInput] = useState({
-name: '',
-hasGluten: true,
-image: ''
-})
+const New = () => {
+  const [petInput, setPetInput] = useState({
+    petType: "",
+    petName: "",
+    gender: "",
+    breed: "",
+    age: "",
+    adoptionStatus: "",
+    image: "",
+    introduction: "",
+    details: "",
+  });
 
-const handleChange = (e) => {
-const value = e.target.value;
-setPetInput({
-...petInput,
-[e.target.name]: value
-});
-}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPetInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-const handleGlutenCheck = (e) => {
-const checked = e.target.checked
-setPetInput({
-...petInput,
-[e.target.name]: checked
-});
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/api/pets", petInput);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-const handleSubmit = async (e) => {
-e.preventDefault()
-const URL = `${process.env.REACT_APP_BACKEND_URI}/pets`
-console.log('pet input', petInput)
-const response = await fetch(URL, {
-method: 'POST',
-headers: {'Content-Type': 'application/json'},
-body: JSON.stringify(petInput)
-})
-if (response.status !== 201) console.log('error')
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Pet Type:
+        <select
+          name="petType"
+          value={petInput.petType}
+          onChange={handleChange}
+          required
+        >
+          <option value="">Select Pet Type</option>
+          <option value="Dog">Dog</option>
+          <option value="Cat">Cat</option>
+        </select>
+      </label>
 
-return (
-<form onSubmit={handleSubmit}>
-<input required onChange={handleChange} value={petInput.name} name='name' placeholder='name' />
-<input onChange={handleGlutenCheck} defaultChecked={petInput.hasGluten} value={petInput.hasGluten} name='hasGluten' type='checkbox' />
-<input onChange={handleChange} value={petInput.image} name='image' placeholder='image' />
-<input type='submit' />
-</form>
-)
-}
+      <label>
+        Pet Name:
+        <input
+            type="text"
+            name="petName"
+            value={petInput.petName}
+            onChange={handleChange}
+            required
+        />
+      </label>
 
-export default New 
+      <label>
+        Gender:
+        <select
+          name="gender"
+          value={petInput.gender}
+          onChange={handleChange}
+        >
+          <option value="">Select Gender</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+      </label>
+
+      <label>
+        Breed:
+        <input
+            type="text"
+            name="breed" 
+            value={petInput.breed}
+            onChange={handleChange}
+            required
+        />
+      </label>
+
+      <label>
+        Age:
+        <select
+          name="age"
+          value={petInput.age}
+          onChange={handleChange}
+          required
+        >
+          <option value="">Select Age</option>
+          <option value="6-8 weeks">6-8 weeks</option>
+          <option value="10-12 weeks">10-12 weeks</option>
+          <option value="16-18 weeks">16-18 weeks</option>
+          <option value="under a year">under a year</option>
+          <option value="1 year">1 year</option>
+          <option value="2 years">2 years</option>
+          <option value="3 years">3 years</option>
+          <option value="4 years">4 years</option>
+          <option value="5 years">5 years</option>
+          <option value="6 years">6 years</option>
+          <option value="7 years">7 years</option>
+          <option value="8 years">8 years</option>
+          <option value="9 years">9 years</option>
+          <option value="10 years">10 years</option>
+          <option value="11 years">11 years</option>
+          <option value="12 years">12 years</option>
+          <option value="13 years">13 years</option>
+          <option value="14 years">14 years</option>
+          <option value="15 years">15 years</option>
+          <option value="Senior over 16 years">Senior over 16 years</option>
+        </select>
+      </label>
+
+      <label>
+        Adoption Status:
+        <select
+          name="Adoption Status"
+          value={petInput.adoptionStatus}
+          onChange={handleChange}
+        >
+        <option value=''>Select Adoption Status</option>
+        <option value='Coming Soon'>Coming Soon</option>
+        <option value='Ready to Adopt'>Ready to Adopt</option>
+        <option value='Pending Adoption'>Pending Adoption</option>
+        <option value='Adopted'>Adopted</option>
+        </select>
+      </label>
+
+      <label>
+        Image URL:
+        <input
+          type="text"
+          name="image"
+          value={petInput.image}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label>
+        Introduction:
+        <textarea
+          name="introduction"
+          value={petInput.introduction}
+          onChange={handleChange}
+          required
+        />
+      </label>
+
+      <label>
+        Details:
+        <textarea
+          name="details"
+          value={petInput.details}
+          onChange={handleChange}
+          required
+        />
+      </label>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default New;
+
--- pet-adoption-safe-haven-frontend/src/Components/UpdatePet.js
+++ pet-adoption-safe-haven-frontend/src/Components/UpdatePet.js
@@ -27,14 +27,6 @@ const handleChange = (e) => {
     });
 }
 
-const handleGlutenCheck = (e) => {
-    const checked = e.target.checked
-        setPetInput({
-        ...petInput,
-        [e.target.name]: checked
-    });
-}
-
 const handleSubmit = async (e) => {
     e.preventDefault()
 const response = await fetch(URL, {
@@ -46,31 +38,56 @@ const response = await fetch(URL, {
         navigate(`/pet/${id}`)
 }
 
-const categories = ['Dog', 'Cat', 'Other'];
-
 const display = petInput && (
   <form onSubmit={handleSubmit}>
-    <select required onChange={handleChange} value={petInput.category} name='category'>
-      {categories.map(category => (
-        <option key={category} value={category}>{category}</option>
-      ))}
-    </select>
-    <input required onChange={handleChange} value={petInput.name} name='name' placeholder='Name' />
-    <input onChange={handleGlutenCheck} defaultChecked={petInput.hasGluten} value={petInput.hasGluten} name='hasGluten' type='checkbox' />
-    <input onChange={handleChange} value={petInput.image} name='image' placeholder='image' />
-    <input type='submit' />
+    <select onChange={handleChange} value={petInput.petType} name='Pet Type'>
+        <option value=''>Select Pet Type</option>
+        <option value='dog'>Dog</option>
+        <option value='cat'>Cat</option>
+      </select>
+      <input required onChange={handleChange} value={petInput.name} name='name' placeholder='name' />
+      <select onChange={handleChange} value={petInput.gender} name='Gender'>
+        <option value=''>Select Gender</option>
+        <option value='male'>Male</option>
+        <option value='female'>Female</option>
+      </select>
+      <input required onChange={handleChange} value={petInput.breed} breed='Breed' placeholder='Breed' />
+      <select onChange={handleChange} value={petInput.age} name='Age'>
+        <option value=''>Select Age</option>
+        <option value='Puppy 6-8 weeks'>Puppy 6-8 weeks</option>
+        <option value='Puppy 10-12 weeks'>Puppy 10-12 weeks</option>
+        <option value='Puppy 16-18 weeks'>Puppy 16-18 weeks</option>
+        <option value='Puppy under a year'>Puppy under a year</option>
+        <option value='1 year'>1 year</option>
+        <option value='2 years'>2 years</option>
+        <option value='3 years'>3 years</option>
+        <option value='4 years'>4 years</option>
+        <option value='5 years'>5 years</option>
+        <option value='6 years'>6 years</option>
+        <option value='7 years'>7 years</option>
+        <option value='8 years'>8 years</option>
+        <option value='9 years'>9 years</option>
+        <option value='10 years'>10 years</option>
+        <option value='11 years'>11 years</option>
+        <option value='12 years'>12 years</option>
+        <option value='13 years'>13 years</option>
+        <option value='14 years'>14 years</option>
+        <option value='15 years'>15 years</option>
+        <option value='Senior over 16 years'>Senior over 16 years</option>
+      </select>
+      <select onChange={handleChange} value={petInput.adoptionStatus} name='Adoption Status'>
+        <option value=''>Select Adoption Status</option>
+        <option value='Coming Soon'>Coming Soon</option>
+        <option value='Ready to Adopt'>Ready to Adopt</option>
+        <option value='Pending Adoption'>Pending Adoption</option>
+        <option value='Adopted'>Adopted</option>
+      </select>
+      <input onChange={handleChange} value={petInput.image} name='image' placeholder='image' />
+      <input required onChange={handleChange} value={petInput.introduction} intro='Introduction' placeholder='Introduction' />
+      <input required onChange={handleChange} value={petInput.details} name='Details' placeholder='Details' />
+      <input type='submit' />
   </form>
 );  
-
-const display = petInput && (
-    <form onSubmit={handleSubmit}>
-        <input required onChange={handleChange} value={petInput.category} category=['Dog', 'Cat', 'Other'] />
-        <input required onChange={handleChange} value={petInput.name} name='Name' placeholder='Name' />
-        <input onChange={handleGlutenCheck} defaultChecked={petInput.hasGluten} value={pet.hasGluten} name='hasGluten' type='checkbox' />
-        <input onChange={handleChange} value={petInput.image} name='image' placeholder='image' />
-        <input type='submit' />
-    </form>
-)
 
 return (
     <div>
