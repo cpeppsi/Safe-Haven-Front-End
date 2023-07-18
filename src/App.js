4166878531fba2543eeb97ad1be62a44012b4327
cpeppsi <caepps28@gmail.com>
2023-07-18 13:12:45 -0400

    Importing Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
